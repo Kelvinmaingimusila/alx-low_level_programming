5de033d4f0fb8c009e59962403b3c09762de4790
Kelvinmaingimusila <kevinmaingi470@gmail.com>
2023-02-24 07:10:35 -0800

    sila

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,39 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * print_triangle - prints triangle, followed by a new line
-  *
-  * @size: size of the triangle to be printed
-  *
-  * Return: void
-  */
-
-void print_triangle(int size)
+ * mul - Multiply 2 integers.
+ * @a: An integer to be multiplied with b
+ * @b : An integer to be multipled with a
+ * Return: Return the result of the multiplication
+ */
+int mul(int a, int b)
 {
-if (size > 0)
-{
-int i;
-i = 1;
-while (i <= size)
-{
-int j, k;
-j = k = 0;
-while (j < size - i)
-{
-_putchar(' ');
-j++;
-}
-while (k < i)
-{
-_putchar('#');
-k++;
-}
-i++;
-if (i < size + 1)
-{
-_putchar('\n');
-}
-}
-}
-_putchar('\n');
+	return (a * b);
 }
